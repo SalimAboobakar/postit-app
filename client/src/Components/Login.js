3e5f15390c957b8ed5a65241f967c0757dcfe46a
SalimAboobakar <salim.a.alhafidh@gmail.com>
2025-02-24 13:34:42 +0400

    location and redux-persist

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,3 +1,4 @@
+import { Button, Input } from "reactstrap";
 import img from "../Images/loginImage.jpg";
 import { Container, Row, Col, Form } from "react-bootstrap"; // Import necessary components
 const Login = () => {
@@ -10,15 +11,21 @@ const Login = () => {
           </Row>
 
           <Row>
+            e-mail <br />
+            <Input type="text">e-mail</Input>
+            <Col md={3}></Col>
+          </Row>
+          <br />
+          <Row>
+            password <br />
+            <Input type="text"></Input>
             <Col md={3}></Col>
           </Row>
 
           <Row>
+            <br />
             <Col md={3}></Col>
-          </Row>
-
-          <Row>
-            <Col md={3}></Col>
+            <Button>login</Button>
           </Row>
         </Form>
       </Container>
