ea6db178537c08235410a40730fa75ee14916151
SalimAboobakar <salim.a.alhafidh@gmail.com>
2025-03-04 09:17:44 +0400

    added userslice

--- client/src/Store/store.js
+++ client/src/Store/store.js
@@ -1,5 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import usersReduce from "../Features/UserSlice";
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    user: usersReduce,
+  },
 });
