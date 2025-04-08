3cba319a1d74bdd339c85305bd3fbf7bcb6a03a4
SalimAboobakar <salim.a.alhafidh@gmail.com>
2025-04-08 09:47:41 +0400

    server added register

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import express from "express";
 import cors from "cors";
 import UserModel from "./models/UserModel.js";
+import bcrypt from "bcrypt";
 const app = express();
 
 app.use(express.json());
@@ -13,12 +14,32 @@ app.use(cors());
 const connectString =
   "mongodb+srv://admin:admin@postitcluster.drjjiqf.mongodb.net/postITDb?retryWrites=true&w=majority&appName=PostITCluster";
 
-mongoose.connect(connectString, {
-  useNewUrlParser: true,
+mongoose.connect(connectString);
 
-  useUnifiedTopology: true,
+//API routes
+app.post("/registerUser", async (req, res) => {
+  try {
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+    const hashedpassword = await bcrypt.hash(password, 10);
+
+    const user = UserModel({
+      name: name,
+      email: email,
+      password: hashedpassword,
+    });
+
+    await user.save();
+    res.send({ user: user, msg: "Added" });
+  } catch (error) {
+    res.status(500).json({ error: "An error ocurred" });
+  }
 });
 
+app.post("/login", async (req, res) => {});
+
+app.post("/updateProfile", async (req, res) => {});
 app.listen(3001, () => {
   console.log("You are connected thank you!");
 });
